883e64427ac9a5fbcf1e8507edf6201b3fc92f19
ArnoldG <arnoldg@vrtrix.com>
2021-07-09 17:01:46 +0800

    minor update

--- ordevicevrtrixglove/ordevicevrtrixglove_device.cpp
+++ ordevicevrtrixglove/ordevicevrtrixglove_device.cpp
@@ -479,10 +479,16 @@ void ORDeviceVRTRIXGlove::Bind()
 
             // Setting global values makes weird value when not live
 			FBVector3d globalTranslation = mHardware.GetDefaultT(i);
-			FBVector3d globalTranslationParent = mHardware.GetDefaultT(mHardware.GetChannelParent(i));
+			FBVector3d globalTranslationParent;
+			if (mHardware.GetChannelParent(i) < 0) {
+				globalTranslationParent = mHardware.GetDefaultT(i);
+			}
+			else {
+				globalTranslationParent = mHardware.GetDefaultT(mHardware.GetChannelParent(i));
+			}
 			FBVector3d localTranslation(globalTranslation.mValue[0]- globalTranslationParent.mValue[0], globalTranslation.mValue[1] - globalTranslationParent.mValue[1], globalTranslation.mValue[2] - globalTranslationParent.mValue[2]);
-             mChannels[i].mModelTemplate->DefaultTranslation = localTranslation;
-             mChannels[i].mModelTemplate->DefaultRotation = mHardware.GetDefaultR(i);
+            mChannels[i].mModelTemplate->DefaultTranslation = localTranslation;
+            mChannels[i].mModelTemplate->DefaultRotation = mHardware.GetDefaultR(i);
         }
     }
 
